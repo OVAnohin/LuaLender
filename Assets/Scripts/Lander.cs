01ed3b52c948f131f352d2eef60450c58e7d4360
OVAnohin <oanohin@outlook.com>
2025-12-27 20:28:31 +0400

    Продолжаю учебу.

---
+++ Assets/Scripts/Lander.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Lander : MonoBehaviour
+{
+    [SerializeField] float Force = 700f;
+    [SerializeField] float TurnSpeed = 100f;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            _rigidbody2D.AddForce(Force * transform.up * Time.deltaTime);
+        }
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            _rigidbody2D.AddTorque(TurnSpeed * Time.deltaTime);
+        }
+
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            _rigidbody2D.AddTorque(-TurnSpeed * Time.deltaTime);
+        }
+    }
+}
